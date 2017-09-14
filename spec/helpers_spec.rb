2da4e90e0d9ff86144ea4949ad61496382d47d81
Mariel F <marielbeth@gmail.com>
2017-09-14 00:29:12 +0000

    automatically backed up by learn

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -7,7 +7,7 @@ describe "Helpers" do
       session = {
         :user_id => 1
       }
-      binding.pry
+      # binding.pry
       expect(Helpers.current_user(session)).to be_an_instance_of(User)
     end
 

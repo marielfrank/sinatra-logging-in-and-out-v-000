fa05679b78314ec98ccd5d6ad4e9ca80e9562080
Mariel F <marielbeth@gmail.com>
2017-09-14 00:30:46 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     User.find_by(id: session[:user_id])
   end
 
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
     !!session[:user_id] == current_user(session).id
   end
 end
